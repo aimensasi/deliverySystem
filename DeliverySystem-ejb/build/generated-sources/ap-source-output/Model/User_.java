2a5d3956c8bbbdd1b23086e5b36b0f9c2094e1cb
AIMEN .S.A.SASI <aimen.s.a.sasi@AIMENs-MacBook-Air-2.local>
2019-03-19 06:44:53 +0800

    init project

---
+++ DeliverySystem-ejb/build/generated-sources/ap-source-output/Model/User_.java
@@ -0,0 +1,17 @@
+package Model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-19T06:42:42")
+@StaticMetamodel(User.class)
+public class User_ { 
+
+    public static volatile SingularAttribute<User, String> password;
+    public static volatile SingularAttribute<User, String> role;
+    public static volatile SingularAttribute<User, String> name;
+    public static volatile SingularAttribute<User, Integer> id;
+    public static volatile SingularAttribute<User, String> email;
+
+}
---
+++ DeliverySystem-ejb/src/java/Model/User.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author aimen.s.a.sasi
+ */
+@Entity
+@Table(name = "user")
+@XmlRootElement
+@NamedQueries({
+  @NamedQuery(name = "User.findAll", query = "SELECT u FROM User u")
+  , @NamedQuery(name = "User.findById", query = "SELECT u FROM User u WHERE u.id = :id")
+  , @NamedQuery(name = "User.findByName", query = "SELECT u FROM User u WHERE u.name = :name")
+  , @NamedQuery(name = "User.findByEmail", query = "SELECT u FROM User u WHERE u.email = :email")
+  , @NamedQuery(name = "User.findByPassword", query = "SELECT u FROM User u WHERE u.password = :password")
+  , @NamedQuery(name = "User.findByRole", query = "SELECT u FROM User u WHERE u.role = :role")})
+public class User implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+  @Id
+  @Basic(optional = false)
+  @NotNull
+  @Column(name = "id")
+  private Integer id;
+  
+  @Basic(optional = false)
+  @NotNull
+  @Size(min = 1, max = 255)
+  @Column(name = "name")
+  private String name;
+  
+  @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+  @Basic(optional = false)
+  @NotNull
+  @Size(min = 1, max = 255)
+  @Column(name = "email")
+  private String email;
+  
+  @Size(max = 255)
+  @Column(name = "password")
+  private String password;
+  
+  @Size(max = 255)
+  @Column(name = "role")
+  private String role;
+
+  public User() {
+  }
+
+  public User(Integer id) {
+    this.id = id;
+  }
+
+  public User(Integer id, String name, String email) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getRole() {
+    return role;
+  }
+
+  public void setRole(String role) {
+    this.role = role;
+  }
+
+  @Override
+  public int hashCode() {
+    int hash = 0;
+    hash += (id != null ? id.hashCode() : 0);
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    // TODO: Warning - this method won't work in the case the id fields are not set
+    if (!(object instanceof User)) {
+      return false;
+    }
+    User other = (User) object;
+    if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "Model.User[ id=" + id + " ]";
+  }
+  
+}
